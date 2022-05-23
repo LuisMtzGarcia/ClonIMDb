534ae423448eb2eaaba83a438bbabed8627240a1
LuisMtzGarcia <ing.luismtzgarcia@gmail.com>
2022-05-23 13:07:11 -0500

    Creara el view para mostrar todas las peliculas
    Este view mostrara todas las peliculas basandose en el modelo Pelicula.

--- appIMDb/views.py
+++ appIMDb/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+from .models import Pelicula
+
 def index(request):
-    """La home page para la app."""
+    """ La home page para la app. """
     return render(request, 'appIMDb/index.html')
+
+def peliculas(request):
+    """ Muestra todas las peliculas. """
+    peliculas = Pelicula.objects.order_by('titulo')
+
+    context = {'peliculas': peliculas}
+    return render(request, 'appIMDb/peliculas.html', context)
