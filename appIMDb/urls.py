67d20dffe4486f47b49c5c79a7e8e0a364e258e2
LuisMtzGarcia <ing.luismtzgarcia@gmail.com>
2022-05-23 12:12:00 -0500

    Se aniadira un patron URL para mostrar todas las peliculas

--- appIMDb/urls.py
+++ appIMDb/urls.py
@@ -8,4 +8,6 @@ app_name = 'appIMDb'
 urlpatterns = [
     # Home page
     path('', views.index, name='index')
+    # Pagina que muestra todas las peliculas.
+    path('peliculas/', views.peliculas, name='peliculas'),
 ]
