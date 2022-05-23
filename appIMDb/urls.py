b831bcda2ebcf4579710e6e23074c11d6659895a
LuisMtzGarcia <ing.luismtzgarcia@gmail.com>
2022-05-23 11:48:17 -0500

    Creara el archivo urls.py dentro de la app
    Se creo el archivo urls.py dentro de la app para definir los patrones URL.
    Se agrego el primer patron para la home page.

---
+++ appIMDb/urls.py
@@ -0,0 +1,11 @@
+"""Define los patrones URL para la app, appIMDb."""
+
+from django.urls import path
+
+from . import views
+
+app_name = 'appIMDb'
+urlpatterns = [
+    # Home page
+    path('', views.index, name='index')
+]
