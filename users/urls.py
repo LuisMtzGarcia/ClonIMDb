ef5b792e51b363fd27c00c02ba087f3032a10988
LuisMtzGarcia <ing.luismtzgarcia@gmail.com>
2022-05-23 19:21:16 -0500

    Creara el view para el registro de usuario, se crea su patron url
    Se crea el view para el formulario de registro de usuario, y se crea su
    respecto patron url.
    
    El archivo html para el template de registro es creado pero esta vacio.

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,12 @@
 
 from django.urls import path, include
 
+from . import views
+
 app_name = "urls"
 urlpatterns = [
     # Incluye las urls de auth default.
     path('', include('django.contrib.auth.urls')),
+    # Pagina para crear una cuenta.
+    path('registro/', views.registro, name='registro'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def registro(request):
+    """Registro para un nuevo usuario."""
+
+    if request.method != 'POST':
+        # Muestra el formulario de registro en blanco.
+        form = UserCreationForm()
+    else:
+        # Procesa el formulario llenado.
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            nuevoUsuario = form.save()
+            # Loggea al usuario y lo redirecciona a la pagina principal
+            login(request, nuevoUsuario)
+            return redirect('appIMDb:index')
+
+    # Recarga la  pagina y muestra un formulario en blanco, o un formulario
+    # que no sea valido.
+    context = {'form': form}
+    return render(request, 'registration/registro.html', context)
