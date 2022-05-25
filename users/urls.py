a8b864b4f9bc9a93c44d868f44a702093420e059
LuisMtzGarcia <ing.luismtzgarcia@gmail.com>
2022-05-25 14:31:17 -0500

    Creara un template placeholder para lista fav, y patron url para lista Fav
    Se crea un template vacio que mostrara eventualmente la lista de peliculas
    marcadas como favoritas por el usuario.
    
    Asimismo, se crea un patron url para la view donde procesara esa lista
    de favoritos y la pasara a este template mencionado.

--- users/urls.py
+++ users/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     # Pagina para crear una cuenta.
     path('registro/', views.registro, name='registro'),
+    # Pagina para visualizar las peliculas favoritas.
+    path('perfil/favoritas', views.listaFav, name='listaFav'),
     # Pagina para agregar a favoritos.
     path('fav/<int:pelicula_id>/', views.marcarFav, name='marcarFav', ),
 ]
