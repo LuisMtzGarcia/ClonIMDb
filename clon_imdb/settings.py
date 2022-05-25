6df8de8bf1603ef5798387260cde971c953fc7bf
LuisMtzGarcia <ing.luismtzgarcia@gmail.com>
2022-05-25 18:17:14 -0500

    Agregara un ALLOWED_HOSTS en settings.py
    Se agrega el host '127.0.0.1' a los hosts permitidos, es el host
    local que me encuentro usando.

--- clon_imdb/settings.py
+++ clon_imdb/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7#+in3-@g171v8-%e3_a*#7a!3x%#f@y$og**z8eqqq$uttc9p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
