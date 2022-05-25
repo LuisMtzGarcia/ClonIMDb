df7e02ce5770d70341d0462e74ba0d0b492327e7
LuisMtzGarcia <ing.luismtzgarcia@gmail.com>
2022-05-25 14:25:15 -0500

    Creara view en users para marcar Favorito
    Este view permitira a un usuario el marcar como favorita una pelicula.
    Si el usuario ha marcado antes esa pelicula como favorita,
    el view entonces la desmarcara.
    Si no, la marcara como favorita.

--- users/views.py
+++ users/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+
+from appIMDb.models import Pelicula
 
 def registro(request):
     """Registro para un nuevo usuario."""
@@ -22,3 +26,17 @@ def registro(request):
     # que no sea valido.
     context = {'form': form}
     return render(request, 'registration/registro.html', context)
+
+@login_required
+def marcarFav(request, pelicula_id):
+    """Usuario marca una pelicula como de sus favoritas."""
+
+    pelicula = get_object_or_404(Pelicula, id=pelicula_id)
+
+    if pelicula.favoritos.filter(id=request.user.id).exists():
+        # Elimina la pelicula de las favoritas del usuario
+        pelicula.favoritos.remove(request.user)
+    else:
+        pelicula.favoritos.add(request.user)
+
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
